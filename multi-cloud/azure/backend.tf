85fc8a69782801bf9fe1c63cfdf8a126e078efc2
Tayo <tayo.ofere@gmail.com>
2025-12-08 02:18:47 +0100

    Modify further

--- multi-cloud/azure/backend.tf
+++ multi-cloud/azure/backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "rg-tfstate"
-    storage_account_name = "emartstorageaccount201"
-    container_name = "emartstoragecontainer201"
-    key = "terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name = "rg-tfstate"
+#     storage_account_name = "emartstorageaccount201"
+#     container_name = "emartstoragecontainer201"
+#     key = "terraform.tfstate"
+#   }
+# }
