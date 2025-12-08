572017a5a85a8641e63dfc3ef9c16b0a7d88a74c
Tayo <tayo.ofere@gmail.com>
2025-12-08 07:24:04 +0100

    FIx errors in outputs of VM

--- multi-cloud/azure/modules/vm/outputs.tf
+++ multi-cloud/azure/modules/vm/outputs.tf
@@ -1,7 +1,3 @@
 output "vm_id" {
-  value = azurerm_virtual_machine.main.id
-}
-
-output "vm_zones" {
-  value = azurerm_virtual_machine.main.zones
+  value = azurerm_linux_virtual_machine.main.id
 }
